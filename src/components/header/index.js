c841f5c52846683ec2852f7337e0482a2e9009c1
sdfc@koncero.com <sfdc@koncero.com>
2021-03-26 14:04:39 +0530

    fixed gatsby-manifest

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,10 @@
 import React from "react"
 import styl from "./header.module.scss"
-// import Logo from "/content/assets/DarshanDevLogoWhitesvg.svg"
 
 export default function Header() {
   return (
     <header style={{ alignItems: "center" }}>
       <nav>
-        {/* <Logo className={styl.logo} /> */}
         <ul className={styl.navList}>
           <li className={styl.navItem}>Dev Blog</li>
           <li className={styl.navItem}>Dev Blog1</li>
