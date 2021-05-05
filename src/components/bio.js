b431becdb56d77a6d0e3b4722bcb7b32fec3729d
sdfc@koncero.com <sfdc@koncero.com>
2021-05-05 15:59:56 +0530

    removed pic

--- src/components/bio.js
+++ src/components/bio.js
@@ -41,16 +41,16 @@ const Bio = () => {
 
   return (
     <div className="bio">
-      {avatar && (
-        // <Image
-        //   fixed={avatar}
-        //   alt={author?.name || ``}
-        //   className="bio-avatar"
-        //   imgStyle={{
-        //     borderRadius: `50%`,
-        //   }}
-        // />
-      )}
+      {/* {avatar && (
+        <Image
+          fixed={avatar}
+          alt={author?.name || ``}
+          className="bio-avatar"
+          imgStyle={{
+            borderRadius: `50%`,
+          }}
+        />
+      )} */}
       {author?.name && (
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
